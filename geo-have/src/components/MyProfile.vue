f4fc2ab6832c4ddbdba6957351b69137800eb603
Sara Gylden <saragbusch@live.dk>
2024-06-14 10:44:53 +0200

    Delete user works
    Delete user + taking to landingpage and sign out

--- geo-have/src/components/MyProfile.vue
+++ geo-have/src/components/MyProfile.vue
@@ -7,18 +7,29 @@
 
 <script>
 import { ref } from 'vue';
-import { getAuth, deleteUser } from "firebase/auth";
+import { getAuth, deleteUser, signOut } from "firebase/auth";
+import { useRouter } from 'vue-router';
 
 export default {
   setup() {
     const auth = getAuth();
     const user = ref(auth.currentUser);
+    const router = useRouter();
 
     const deleteUserAccount = () => {
       if (user.value) {
         deleteUser(user.value)
           .then(() => {
             console.log("User Account Deleted Successfully");
+            // Log out the user
+            signOut(auth)
+              .then(() => {
+                // Redirect to landing page
+                router.push('/');
+              })
+              .catch((signOutError) => {
+                console.error("Error signing out:", signOutError);
+              });
           })
           .catch((error) => {
             console.error("Error deleting user:", error);
