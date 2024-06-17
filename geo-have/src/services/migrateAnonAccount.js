3fd76b001744d0c95189f4f10196d5b968f6fa76
Ezra Andersen <stardustezra@gmail.com>
2024-06-17 12:44:43 +0200

    fjern ubrugt kode

--- geo-have/src/services/migrateAnonAccount.js
+++ geo-have/src/services/migrateAnonAccount.js
@@ -1,11 +1,4 @@
-import {
-  EmailAuthProvider,
-  FacebookAuthProvider,
-  GoogleAuthProvider,
-  getAuth,
-  linkWithCredential,
-  linkWithPopup,
-} from "firebase/auth";
+import { GoogleAuthProvider, getAuth, linkWithPopup } from "firebase/auth";
 
 // Function to migrate anonymous account to Google account using a popup
 export const migrateAnonAccountToGoogle = async () => {
@@ -17,38 +10,5 @@ export const migrateAnonAccountToGoogle = async () => {
     })
     .catch((error) => {
       console.log(error); // Log any errors
-    });
-};
-
-// Function to migrate anonymous account to a specified provider (Google, email, Facebook)
-export const migrateAnonAccount = async (provider, data) => {
-  let credential = null;
-
-  switch (provider) {
-    case "google":
-      // Google sign-in provider
-      credential = GoogleAuthProvider.credential(data.idToken); // Create Google credential using ID token
-      break;
-    case "email":
-      // Email sign-in provider
-      credential = EmailAuthProvider.credential(data.email, data.password); // Create email credential using email and password
-      break;
-    case "facebook":
-      // Facebook sign-in provider
-      credential = FacebookAuthProvider.credential(data.accessToken); // Create Facebook credential using access token
-      break;
-    default: {
-      console.error("Invalid provider"); // Handle invalid provider case
-      return;
-    }
-  }
-  const auth = getAuth(); // Get current authentication instance
-  linkWithCredential(auth.currentUser, credential) // Link the current user with the created credential
-    .then((usercred) => {
-      const user = usercred.user; // Get the upgraded user
-      console.log("Anonymous account successfully upgraded", user); // Log success message
-    })
-    .catch((error) => {
-      console.log("Error upgrading anonymous account", error); // Log any errors
     });
 };
